f43e50956b94f97efffa1a56ea6fb7408094f1a4
ChoiHyunMin <chl1519@naver.com>
2022-03-07 00:10:25 +0900

    0306 - 댓글 추출

--- crawl.py
+++ crawl.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 from urllib import parse
 from urllib.request import urlopen
@@ -26,32 +27,44 @@ def get_news(keyword: str) -> List:
         for link in links:
             if link.get_text() == '네이버뉴스':
                 url_list.append(link["href"])
-                
+    
+    # print(url_list)
     return url_list
 
-def scrap_and_parse():
-    url_list = 'https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=101&oid=001&aid=0013007145'
-    headers = {
-                    "authority": "apis.naver.com",
-                    "method": "GET",
-                    "scheme": "https",
-                    "accept": "*/*",
-                    "accept-encoding": "gzip, deflate, br",
-                    "referer": "https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid=014&aid=0004796402",
-                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-                }
+def scrap_and_parse(url_list):
     
-    req = requests.get(url_list, headers=headers)
-    print(req.text)
-    
-    
-    # except:
-    #     page = urlopen(url_list)
-    #     page_encode = page.read().decode('cp949')
-    #     print(page_encode)  
+    for url in url_list:
+        url_id = url.split('aid=')[-1]
+        link = 'https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_economy&pool=cbox5&_cv=20220217154255&_callback=jQuery1124029040860222038956_1646227945222&lang=ko&country=KR&objectId=news014,0004796402&categoryId=&pageSize=19192&indexSize=&groupId=&listType=OBJECT&pageType=more&page=1&refresh=true&sort=FAVORITE&includeAllStatus=true&_={url_id}'
+
+        headers = {
+            "authority": "apis.naver.com",
+            "method": "GET",
+            "scheme": "https",
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate, br",
+            "referer": "https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid=014&aid=0004796402",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+        }
+
+        res = requests.get(url, headers=headers)
+        
+
+        jquery = res.text[res.text.find("(")+1:-2]
+        print(jquery)
+
+        # jquery = json.loads(jquery)
+        time.sleep(1)
+        # commentList = jquery["result"]["commentList"]
+
+        # print(len(commentList))
+        # for comment in commentList:
+        #     print(comment['contents'])
   
 def main():
-    scrap_and_parse()
+    url_list = get_news("국토부")
+    scrap_and_parse(url_list)
+
     
     
     
@@ -61,31 +74,3 @@ if __name__ == '__main__':
 
 
 
-
-# url = 'https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_economy&pool=cbox5&_cv=20220217154255&_callback=jQuery1124029040860222038956_1646227945222&lang=ko&country=KR&objectId=news014,0004796402&categoryId=&pageSize=19192&indexSize=&groupId=&listType=OBJECT&pageType=more&page=1&refresh=true&sort=FAVORITE&includeAllStatus=true&_={s14}'
-# # url = 'https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_economy&pool=cbox5&_cv=20220217154255&_callback=jQuery112406935308254980357_1646572243606&lang=ko&country=KR&objectId=news014,0004796402&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&pageType=more&page=1&refresh=true&sort=FAVORITE&includeAllStatus=true&_=1646572243609'
-
-# headers = {
-#     "authority": "apis.naver.com",
-#     "method": "GET",
-#     "scheme": "https",
-#     "accept": "*/*",
-#     "accept-encoding": "gzip, deflate, br",
-#     "referer": "https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid=014&aid=0004796402",
-#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-# }
-
-
-# res = requests.get(url, headers=headers)
-# print(res)
-# print(res.text)
-
-# jquery = res.text[res.text.find("(")+1:-2]
-# jquery = json.loads(jquery)
-# commentList = jquery["result"]["commentList"]
-# print(commentList)
-
-
-# print(len(commentList))
-# for comment in commentList:
-#     print(comment['contents'])
