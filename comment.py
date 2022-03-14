65b9681865aca4dfe977c3c64804eacaf47662b4
ChoiHyunMin <chl1519@naver.com>
2022-03-15 00:14:31 +0900

    0315 - headers 수정, 스크랩/파싱 분리

--- comment.py
+++ comment.py
@@ -1,12 +1,28 @@
-from collections import defaultdict
 import requests
 import json
 import time
+import logging
+from collections import defaultdict
 from bs4 import BeautifulSoup
 from urllib import parse
 from typing import List
 
 
+
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)s] : %(message)s',
+    datefmt='%Y-%m-%d %I:%M:%S',
+    level=logging.DEBUG
+)
+headers = {
+    "authority": "apis.naver.com",
+    "method": "GET",
+    "scheme": "https",
+    "accept": "*/*",
+    "accept-encoding": "gzip, deflate, br",
+    # "referer": f"https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid={url_oid}&aid={url_id}",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+}
 
 def get_news(keyword: str) -> List:
     """Get news list enclose the keyword
@@ -27,55 +43,66 @@ def get_news(keyword: str) -> List:
             if link.get_text() == "네이버뉴스":
                 url_list.append(link["href"])
     
-    # print(url_list)
+    # logging.info(url_list)
     return url_list
 
-def scrap_and_parse(url_list):
-    """url 이동 후 댓글 스크랩 후 파싱
+def scrap(url):
+    """url 이동 후 댓글 스크랩
 
     Args:
         url_list (List): 기사 리스트
+    return: dict
     """
-    for url in url_list:
-        url_id = url.split("aid=")[-1]
-        # print(url_id)
-        link = f"https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_economy&pool=cbox5&_cv=20220313225543&_callback=jQuery1124029040860222038956_1646227945222&lang=ko&country=KR&objectId=news014,0004796402&categoryId=&pageSize=30000&indexSize=&groupId=&listType=OBJECT&pageType=more&page=&refresh=true&sort=FAVORITE&includeAllStatus=true&_={url_id}"
+    global headers
+    title = requests.get(url, headers=headers)
+    bs = BeautifulSoup(title.text, "lxml")
+    title = bs.find("h3", attrs={"id":"articleTitle"}).text
+    logging.debug(title)
+    
+    pre_url = url.split("oid=")[1].split("&")
+    url_oid = pre_url[0]
+    url_id = pre_url[1].split("aid=")[-1]
 
-        headers = {
-            "authority": "apis.naver.com",
-            "method": "GET",
-            "scheme": "https",
-            "accept": "*/*",
-            "accept-encoding": "gzip, deflate, br",
-            "referer": f"https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid=014&aid={url_id}",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-        }
+    link = f"https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_economy&pool=cbox5&_cv=20220313225543&_callback=jQuery1124029040860222038956_1646227945222&lang=ko&country=KR&objectId=news{url_oid}%2C{url_id}&categoryId=&pageSize=30000&indexSize=&groupId=&listType=OBJECT&pageType=more&page=&refresh=true&sort=FAVORITE&includeAllStatus=true&_={url_id}"
+    headers = {
+        "authority": "apis.naver.com",
+        "method": "GET",
+        "scheme": "https",
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "referer": f"https://news.naver.com/main/read.naver?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=101&oid={url_oid}&aid={url_id}",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    }
+    res = requests.get(link, headers=headers)
+    jquery = res.text[res.text.find("(")+1:-2]
 
-        res = requests.get(link, headers=headers)
-        jquery = res.text[res.text.find("(")+1:-2]
-        jquery = json.loads(jquery)
-        
-        commentList = jquery["result"]["commentList"]
-        
-        result = defaultdict(list)
+    return title, jquery
 
-        for comment in commentList:
-            if comment["contents"]:
-                result[link].append(comment["contents"])
 
-        print(result)
-        time.sleep(1)
+def parsing(title, jquery):
+    jquery = json.loads(jquery)
+    commentList = jquery["result"]["commentList"]
+    
+    result = defaultdict(list)
+    for comment in commentList:
+        if comment["contents"]:
+            result[title].append(comment["contents"])
+        logging.info(result)
+    time.sleep(1)
+    return result
         
 
   
 def main():
     url_list = get_news("국토부")
-    scrap_and_parse(url_list)
-
+    for url in url_list:
+        title, jquery = scrap(url)
+        result = parsing(title, jquery)
+    
     
 if __name__ == '__main__':
     main()
-
+    
 
 
 
